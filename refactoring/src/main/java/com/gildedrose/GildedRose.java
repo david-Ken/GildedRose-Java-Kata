01139a5fdb8d7b671825aeaffa12c04827939ba9
david <davidmyken@gmail.com>
2018-01-21 13:52:07 +0100

    Update GildedRose.java

--- refactoring/src/main/java/com/gildedrose/GildedRose.java
+++ refactoring/src/main/java/com/gildedrose/GildedRose.java
@@ -3,7 +3,7 @@ package com.gildedrose;
 
 
 class GildedRose {
-    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+        private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 	private static final String AGED_BRIE = "Aged Brie";
 	private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
 	
